843c044fc4a72c603101175fcd18be375e431938
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-04 20:34:01 -0500

    Ejemplo de uso de async para el manejo de valores de retorno de un Job asíncrono.

---
+++ Coroutines/1_Coroutines_basics/src/main/kotlin/Async.kt
@@ -0,0 +1,33 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
+
+fun main() {
+    runBlocking {
+        val firstDeferred = async { getFirstValue() }
+        val secondDeferred = async { getSecondValue() }
+
+        println("Doing some processing here...")
+        delay(500L)
+        println("Waiting for values")
+        val firstValue = firstDeferred.await()
+        val secondValue = secondDeferred.await()
+
+        println("The total is ${firstValue + secondValue}")
+    }
+}
+
+suspend fun getFirstValue(): Int {
+    delay(1000L)
+    val value = Random.nextInt(100)
+    println("Returning first value: $value")
+    return value
+}
+
+suspend fun getSecondValue(): Int {
+    delay(2000L)
+    val value = Random.nextInt(100)
+    println("Returning second value: $value")
+    return value
+}
