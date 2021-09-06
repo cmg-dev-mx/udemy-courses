0b72daefa0de52670ff5e41bc18be29880a512b2
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-05 20:55:51 -0500

    Carga de imagen desde internet por medio de corrutinas en el scope IO

--- Coroutines/ImageProcessingCoroutines/app/src/main/java/mx/dev/shell/android/imageprocessingcoroutines/MainActivity.kt
+++ Coroutines/ImageProcessingCoroutines/app/src/main/java/mx/dev/shell/android/imageprocessingcoroutines/MainActivity.kt
@@ -1,14 +1,57 @@
 package mx.dev.shell.android.imageprocessingcoroutines
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    companion object {
+        private const val IMAGE_URL =
+            "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    }
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var imageView: ImageView
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
+        imageView = findViewById(R.id.imageView)
+
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO) {
+                getOriginalBitmap()
+            }
+
+            val originalBitmap = originalDeferred.await()
+
+            loadImage(originalBitmap)
+        }
+    }
+
+    private fun getOriginalBitmap() = URL(IMAGE_URL)
+        .openStream()
+        .use {
+            BitmapFactory.decodeStream(it)
+        }
+
+    private fun loadImage(bmp: Bitmap) {
+        progressBar.visibility = View.GONE
+        imageView.setImageBitmap(bmp)
+        imageView.visibility = View.VISIBLE
     }
 }
