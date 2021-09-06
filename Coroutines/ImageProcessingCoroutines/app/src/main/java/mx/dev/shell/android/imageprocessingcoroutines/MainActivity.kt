449d3e6f0c1cb2392aff525346ccf1551890422a
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-05 20:32:18 -0500

    Creación de componentes y vista base para el ejemplo de manejo de procesos en segundo plano

--- Coroutines/ImageProcessingCoroutines/app/src/main/java/mx/dev/shell/android/imageprocessingcoroutines/MainActivity.kt
+++ Coroutines/ImageProcessingCoroutines/app/src/main/java/mx/dev/shell/android/imageprocessingcoroutines/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
