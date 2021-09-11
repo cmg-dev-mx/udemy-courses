720667fca75d7da8ae2c04d38553393b613e93ba
cmg.dev.mx@gmail.com <shell.one.core@gmail.com>
2021-09-11 14:39:09 -0500

    Creación de ViewModel para el fragmento Login

--- Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/vm/LoginViewModel.kt
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/vm/LoginViewModel.kt
@@ -6,9 +6,14 @@ import androidx.lifecycle.ViewModel
 class LoginViewModel: ViewModel() {
 
     val signupComplete = MutableLiveData<Boolean>()
+    val loginComplete = MutableLiveData<Boolean>()
     val error = MutableLiveData<String>()
 
     fun signup(user: String, password: String, info: String) {
+
+    }
+
+    fun login(user: String, password: String) {
 
     }
 }
