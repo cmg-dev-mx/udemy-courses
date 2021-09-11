cf9013a9243c131fb6db7b3026c00975b1bd2b56
cmg.dev.mx@gmail.com <shell.one.core@gmail.com>
2021-09-11 14:27:49 -0500

    Creación de ViewModel para el fragmento Signup

--- Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/view/SignupFragment.kt
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/view/SignupFragment.kt
@@ -5,14 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import mx.dev.shell.android.coroutinesroom.R
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import mx.dev.shell.android.coroutinesroom.databinding.FragmentSignupBinding
+import mx.dev.shell.android.coroutinesroom.vm.LoginViewModel
 
 class SignupFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignupBinding
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+    ): View {
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        setupListeners()
+        observeViewModel()
+        return binding.root
+    }
+
+    private fun setupListeners() = binding.apply {
+        signupBtnSignup.setOnClickListener { onSignup(it) }
+        signupBtnLogin.setOnClickListener { onGotoLogin(it) }
+    }
+
+    private fun observeViewModel() = viewModel.apply {
+        signupComplete.observe(this@SignupFragment as LifecycleOwner) { isComplete ->
+            // TODO
+        }
+        error.observe(this@SignupFragment as LifecycleOwner) { isError ->
+            // TODO
+        }
+    }
+
+    private fun onSignup(v: View) {
+        val action = SignupFragmentDirections.actionSignupFragmentToMainActivity()
+        Navigation.findNavController(v).navigate(action)
+    }
+
+    private fun onGotoLogin(v: View) {
+        val action = SignupFragmentDirections.actionSignupFragmentToLoginFragment()
+        Navigation.findNavController(v).navigate(action)
     }
 }
---
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/vm/LoginViewModel.kt
@@ -0,0 +1,14 @@
+package mx.dev.shell.android.coroutinesroom.vm
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+    val signupComplete = MutableLiveData<Boolean>()
+    val error = MutableLiveData<String>()
+
+    fun signup(user: String, password: String, info: String) {
+
+    }
+}
