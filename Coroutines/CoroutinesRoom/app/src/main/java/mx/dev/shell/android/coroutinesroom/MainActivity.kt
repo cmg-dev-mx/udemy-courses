04675720cb38aab8f2b458b54563b736c752b2e2
cmg.dev.mx@gmail.com <shell.one.core@gmail.com>
2021-09-11 00:51:11 -0500

    Creación del proyecto de uso de corrutinas con Base de datos Room

---
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.dev.shell.android.coroutinesroom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
