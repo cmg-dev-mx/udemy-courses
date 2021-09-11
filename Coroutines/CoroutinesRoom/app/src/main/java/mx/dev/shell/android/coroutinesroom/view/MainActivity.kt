a5e5d32c6d341b4b31115ca2f0879a84391e04f2
cmg.dev.mx@gmail.com <shell.one.core@gmail.com>
2021-09-11 01:36:05 -0500

    Construcción de las activities, fragmentos y archivos de navegación de la app

--- Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/view/MainActivity.kt
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package mx.dev.shell.android.coroutinesroom
+package mx.dev.shell.android.coroutinesroom.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.dev.shell.android.coroutinesroom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/view/MainFragment.kt
@@ -0,0 +1,18 @@
+package mx.dev.shell.android.coroutinesroom.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import mx.dev.shell.android.coroutinesroom.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
