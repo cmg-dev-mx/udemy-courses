878ad1cd0a898f481af948f9255b11aa1b54ff40
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-11 22:02:46 -0500

    Implementación de funcionalidad de vista Main

--- Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/view/MainFragment.kt
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/view/MainFragment.kt
@@ -1,5 +1,6 @@
 package mx.dev.shell.android.coroutinesroom.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import com.google.android.material.snackbar.Snackbar
 import mx.dev.shell.android.coroutinesroom.R
 import mx.dev.shell.android.coroutinesroom.databinding.FragmentMainBinding
+import mx.dev.shell.android.coroutinesroom.model.LoginState
 import mx.dev.shell.android.coroutinesroom.vm.MainViewModel
 
 class MainFragment : Fragment() {
@@ -23,6 +26,9 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(layoutInflater, container, false)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.mainTxtUser.text = LoginState.user?.userName
+
         setupListeners()
         observeViewModel()
         return binding.root
@@ -35,20 +41,44 @@ class MainFragment : Fragment() {
 
     private fun observeViewModel() = viewModel.apply {
         logout.observe(this@MainFragment as LifecycleOwner) { logout ->
-
+            showMessage(getString(R.string.main_message_logout))
+            gotoLogin()
         }
         userDeleted.observe(this@MainFragment as LifecycleOwner) { isDeleted ->
-
+            showMessage(getString(R.string.main_message_userDeleted))
+            gotoLogin()
         }
     }
 
     private fun onLogout(v: View) {
-        val action = MainFragmentDirections.actionMainFragmentToLoginActivity()
-        Navigation.findNavController(v).navigate(action)
+        viewModel.logout()
     }
 
     private fun onDelete(v: View) {
+        activity?.let {
+            AlertDialog.Builder(it)
+                .setTitle(getString(R.string.deleteDialog_title))
+                .setMessage(getString(R.string.deleteDialog_question))
+                .setPositiveButton(getString(R.string.deleteDialog_btn_accept)) { _, _ ->
+                    viewModel.deleteUser()
+                }
+                .setNegativeButton(getString(R.string.deleteDialog_btn_cancel), null)
+                .create()
+                .show()
+        }
+    }
+
+    private fun gotoLogin() {
         val action = MainFragmentDirections.actionMainFragmentToLoginActivity()
-        Navigation.findNavController(v).navigate(action)
+        Navigation.findNavController(binding.mainContainer)
+            .navigate(action)
+    }
+
+    private fun showMessage(message: String) {
+        Snackbar.make(
+            binding.mainContainer,
+            message,
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 }
--- Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/vm/MainViewModel.kt
+++ Coroutines/CoroutinesRoom/app/src/main/java/mx/dev/shell/android/coroutinesroom/vm/MainViewModel.kt
@@ -1,18 +1,38 @@
 package mx.dev.shell.android.coroutinesroom.vm
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import mx.dev.shell.android.coroutinesroom.model.LoginState
+import mx.dev.shell.android.coroutinesroom.model.UserDataBase
+import mx.dev.shell.android.coroutinesroom.model.UserDataBase_Impl
 
-class MainViewModel: ViewModel() {
+class MainViewModel(application: Application): AndroidViewModel(application) {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+    private val db by lazy { UserDataBase.getInstance(getApplication()).userDao() }
 
     val logout = MutableLiveData<Boolean>()
     val userDeleted = MutableLiveData<Boolean>()
 
     fun logout() {
-
+        LoginState.logout()
+        logout.value = true
     }
 
     fun deleteUser() {
-
+        coroutineScope.launch {
+            LoginState.user?.let { user ->
+                db.deleteUser(user.id)
+            }
+            withContext(Dispatchers.Main) {
+                LoginState.logout()
+                userDeleted.value = true
+            }
+        }
     }
 }
