7b39a17062ae32d92808d893f263a1f18cc6cb41
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-22 23:27:36 -0500

    Implementación de solución 2 con confinamiento de hilos

---
+++ Coroutines/Shared States/src/main/kotlin/ThreadConfinementFineGrained.kt
@@ -0,0 +1,35 @@
+import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    runBlocking {
+        val counterContext = newSingleThreadContext("CounterContext")
+        var counter = 0
+        withContext(Dispatchers.Default) {
+            massiveRun {
+                withContext(counterContext) {
+                    counter++
+                }
+            }
+        }
+        println("Counter = $counter")
+    }
+}
+
+private suspend fun massiveRun(action: suspend() -> Unit) {
+    val n = 100
+    val k = 1000
+    val time = measureTimeMillis {
+        coroutineScope {
+            repeat(n) {
+                launch {
+                    repeat(k) {
+                        action()
+                    }
+                }
+            }
+        }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
