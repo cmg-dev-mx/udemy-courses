c3a795b2ee82c64d652385da67632baf09d4e526
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-22 20:23:17 -0500

    Implementación de ejemplo de uso de un canal con buffer

---
+++ Coroutines/Channels/src/main/kotlin/BufferedChannel.kt
@@ -0,0 +1,22 @@
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    runBlocking {
+        val channel = Channel<Int>(4)
+        val sender = launch {
+            repeat(10) {
+                channel.send(it)
+                println("Sent $it")
+            }
+        }
+
+        repeat(3) {
+            delay(1000L)
+            println("Received ${channel.receive()}")
+        }
+        sender.cancel()
+    }
+}
