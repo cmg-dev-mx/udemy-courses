3553d0c8930115be9c44ab8274adb0d9b379d80f
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-25 12:54:01 -0500

    Refactor de pruebas del carro con mock del comportamiento del motor

--- Coroutines/OutsideInTddExample/app/src/test/java/mx/dev/shell/android/outsideintddexample/unitTest/CarShould.kt
+++ Coroutines/OutsideInTddExample/app/src/test/java/mx/dev/shell/android/outsideintddexample/unitTest/CarShould.kt
@@ -1,6 +1,8 @@
 package mx.dev.shell.android.outsideintddexample.unitTest
 
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import mx.dev.shell.android.outsideintddexample.Car
 import mx.dev.shell.android.outsideintddexample.Engine
@@ -12,8 +14,21 @@ import org.mockito.Mockito.*
 class CarShould {
 
     private val engine = mock(Engine::class.java)
+    private val car: Car
 
-    private val car = Car(5.0, engine)
+    init {
+        runBlockingTest {
+            `when`(engine.turnOn()).thenReturn(flow {
+                delay(2000)
+                emit(25)
+                delay(2000)
+                emit(50)
+                delay(2000)
+                emit(95)
+            })
+        }
+        car = Car(5.0, engine)
+    }
 
     @get: Rule
     var coroutineTestRule = MainCoroutineScopeRule()
