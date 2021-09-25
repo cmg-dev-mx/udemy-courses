49eb9241812f26710787d2f083e07751d8ff196f
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-09-24 22:36:47 -0500

    Implementación de corrutina en el encendido del motor

--- Coroutines/OutsideInTddExample/app/src/main/java/mx/dev/shell/android/outsideintddexample/Car.kt
+++ Coroutines/OutsideInTddExample/app/src/main/java/mx/dev/shell/android/outsideintddexample/Car.kt
@@ -1,9 +1,15 @@
 package mx.dev.shell.android.outsideintddexample
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 class Car(var fuel: Double, val engine: Engine) {
 
     fun turnOn() {
         fuel -= 0.5
-        engine.turnOn()
+        CoroutineScope(Dispatchers.Main).launch {
+            engine.turnOn()
+        }
     }
 }
--- Coroutines/OutsideInTddExample/app/src/main/java/mx/dev/shell/android/outsideintddexample/Engine.kt
+++ Coroutines/OutsideInTddExample/app/src/main/java/mx/dev/shell/android/outsideintddexample/Engine.kt
@@ -1,12 +1,20 @@
 package mx.dev.shell.android.outsideintddexample
 
+import android.util.Log
+import kotlinx.coroutines.delay
+
 class Engine(
     var temp: Int = 15,
     var isTurnedOn: Boolean = false
 ) {
 
-    fun turnOn() {
+    suspend fun turnOn() {
         isTurnedOn = true
+
+        delay(6000)
+
         temp = 95
+
+        Log.d("Engine", "Engine has turned on")
     }
 }
