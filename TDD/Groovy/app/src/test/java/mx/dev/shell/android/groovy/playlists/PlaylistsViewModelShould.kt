7632a155f6421c328f2b363775300fcf934320b6
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-10-02 15:16:54 -0500

    Implementación de prueba de error de resultado del repository mockeado

--- TDD/Groovy/app/src/test/java/mx/dev/shell/android/groovy/playlists/PlaylistsViewModelShould.kt
+++ TDD/Groovy/app/src/test/java/mx/dev/shell/android/groovy/playlists/PlaylistsViewModelShould.kt
@@ -9,6 +9,7 @@ import mx.dev.shell.android.groovy.utils.BaseUnitTest
 import mx.dev.shell.android.groovy.utils.getValueForTest
 import org.junit.Test
 import org.mockito.Mockito.*
+import java.lang.RuntimeException
 
 @ExperimentalCoroutinesApi
 class PlaylistsViewModelShould: BaseUnitTest() {
@@ -16,6 +17,7 @@ class PlaylistsViewModelShould: BaseUnitTest() {
     private val repository = mock(PlaylistRepository::class.java)
     private val playlists: List<Playlist> = mock(List::class.java) as List<Playlist>
     private val expected = Result.success(playlists)
+    private val exception = RuntimeException("Something went wrong")
 
     init {
 
@@ -33,6 +35,25 @@ class PlaylistsViewModelShould: BaseUnitTest() {
     fun emitsPlaylistsFromRepository() = runBlockingTest {
         val viewModel = mockupSuccessfulCase()
         assertEquals(expected, viewModel.playlists.getValueForTest())
+    }
+
+    @Test
+    fun emitErrorWhenReceiveError() {
+        val viewModel = mockupFailureCase()
+
+        assertEquals(exception, viewModel.playlists.getValueForTest()!!.exceptionOrNull())
+    }
+
+    private fun mockupFailureCase(): PlaylistsViewModel {
+        runBlocking {
+            `when`(repository.getPlaylists()).thenReturn(
+                flow {
+                    emit(Result.failure<List<Playlist>>(exception))
+                }
+            )
+        }
+        val viewModel = PlaylistsViewModel(repository)
+        return viewModel
     }
 
     private fun mockupSuccessfulCase(): PlaylistsViewModel {
