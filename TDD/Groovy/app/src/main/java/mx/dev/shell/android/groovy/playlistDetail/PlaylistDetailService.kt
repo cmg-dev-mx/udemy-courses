65310663c2c0aec9a706d36bb50078c33b28814c
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2021-10-09 18:16:41 -0500

    Implementación de casos de pruebas para la capa de servicio de detalle de playlist

--- TDD/Groovy/app/src/main/java/mx/dev/shell/android/groovy/playlistDetail/PlaylistDetailService.kt
+++ TDD/Groovy/app/src/main/java/mx/dev/shell/android/groovy/playlistDetail/PlaylistDetailService.kt
@@ -1,11 +1,21 @@
 package mx.dev.shell.android.groovy.playlistDetail
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import mx.dev.shell.android.groovy.playlists.PlaylistApi
+import java.lang.RuntimeException
 import javax.inject.Inject
 
-class PlaylistDetailService @Inject constructor() {
+class PlaylistDetailService @Inject constructor(
+    private val api: PlaylistApi
+) {
 
     suspend fun fetchPlaylistDetail(id: String): Flow<Result<PlaylistDetail>> {
-        TODO("Not yet implemented")
+        return flow {
+            emit(Result.success(api.fetchPlaylistById(id)))
+        }.catch {
+            emit(Result.failure(RuntimeException("Something went wrong")))
+        }
     }
 }
--- TDD/Groovy/app/src/main/java/mx/dev/shell/android/groovy/playlists/PlaylistApi.kt
+++ TDD/Groovy/app/src/main/java/mx/dev/shell/android/groovy/playlists/PlaylistApi.kt
@@ -1,10 +1,15 @@
 package mx.dev.shell.android.groovy.playlists
 
+import mx.dev.shell.android.groovy.playlistDetail.PlaylistDetail
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PlaylistApi {
 
     @GET("playlists")
     suspend fun fetchAllPlaylists(): List<PlaylistRaw>
+
+    @GET("playlist-details/{id}")
+    suspend fun fetchPlaylistById(@Path("id") id: String): PlaylistDetail
 
 }
