998da58e8b7e915295e378d5025d9b84d9af795e
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2022-06-22 09:03:41 -0400

    Creación del proyecto Meals App

---
+++ Compose/MealsApp/app/src/main/java/mx/dev/shell/android/mealsapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package mx.dev.shell.android.mealsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.dev.shell.android.mealsapp.ui.theme.MealsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MealsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MealsAppTheme {
+        Greeting("Android")
+    }
+}
